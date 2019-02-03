d02a484f1853d3f8af733d5ccab2e57b23670e01
andeerlb <andeerlb@gmail.com>
2019-02-03 18:55:37 -0200

    adicionado filtro por cidade

--- src/app/pages/rotas-possiveis/rotas-possiveis.service.ts
+++ src/app/pages/rotas-possiveis/rotas-possiveis.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment as env } from '../../../environments/environment';
 import { EnumGroupByPossibleRouter } from '../../shared/models/enum-group-by-possible-router.model';
+import { City } from 'src/app/shared/models/city.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +12,20 @@ export class RotasPossiveisService {
 
   constructor(private http:HttpClient) { }
 
-  list(groupBy: EnumGroupByPossibleRouter, radius: any, showIdleCollaborator: any, showOutOfReachByCollaborator): Observable<any> {
+  list(groupBy: EnumGroupByPossibleRouter, radius: any, showIdleCollaborator: any, showOutOfReachByCollaborator, city: any): Observable<any> {
     let parameters = new HttpParams();
     parameters = parameters.append('radius', radius);
     parameters = parameters.append('groupBy', groupBy);
     parameters = parameters.append('idlbeCollaborator', showIdleCollaborator);
     parameters = parameters.append('outOfReachByCollaborator', showOutOfReachByCollaborator);
+
+    if(city !== null && city !== undefined) {
+      parameters = parameters.append('city', city);
+    }
     return this.http.get(`${env.api}/routers/possibles`, {params: parameters});
+  }
+
+  listOfCities(): Observable<City> {
+    return this.http.get(`${env.api}/city/all`) as Observable<City>;
   }
 }
