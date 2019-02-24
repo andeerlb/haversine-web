20d0f4e3c5032f5893059ed2a692d0754c0fc809
andeerlb <andeerlb@gmail.com>
2019-02-24 05:26:51 -0300

    pagination

--- src/app/shared/components/custom-data-table/custom-data-table.component.ts
+++ src/app/shared/components/custom-data-table/custom-data-table.component.ts
@@ -26,6 +26,8 @@ export class CustomDataTableComponent implements OnInit, OnDestroy {
   showDataTable: boolean = false;
   pageable: Pageable;
   pages: string[] = [];
+  showPages: string[] = [];
+  totalElements: number;
 
   constructor(private _router: Router, private _cadastroService: CadastroService, private _route: ActivatedRoute) { }
 
@@ -39,13 +41,96 @@ export class CustomDataTableComponent implements OnInit, OnDestroy {
     this.showDataTable = false;
   }
 
-  public pagination(page: string) {
+  public pagination(page: string, index: number) {
     this.pageable.page = page;
     this.dataTableGetAll();
+
+    let diference = (index > this.showPages.length) ? index - this.showPages.length : this.showPages.length - index;
+    this.maxNumbers(diference);
+    this.minNumbers(diference, page);
   }
+
+  private minNumbers(diferente: number, page: string): void {   
+    if((+this.showPages[0] - 1) < 0 || +page === 0){
+      return;
+    }
+
+    if(+page === 1){
+      this.showPages.pop();
+      this.showPages.unshift((+this.showPages[0] - 1).toString());
+      return;
+    } else if(+page === 2) {
+      this.showPages.pop();
+      this.showPages.unshift((+this.showPages[0] - 1).toString());
+      return;
+    }
+    
+    switch (diferente) {
+      case 7:
+        this.showPages.pop();
+        this.showPages.pop();
+        this.showPages.pop();
+        this.showPages.unshift((+this.showPages[0] - 1).toString());
+        this.showPages.unshift((+this.showPages[0] - 1).toString());
+        this.showPages.unshift((+this.showPages[0] - 1).toString());
+      break;
+      case 6:
+        this.showPages.pop();
+        this.showPages.pop();
+        this.showPages.unshift((+this.showPages[0] - 1).toString());
+        this.showPages.unshift((+this.showPages[0] - 1).toString());
+      break;
+      case 5:
+        this.showPages.pop();
+        this.showPages.unshift((+this.showPages[0] - 1).toString());
+      break;
+    }
+  }
+
+  private maxNumbers(diferente: number): void {
+    switch (diferente) {
+      case 1:
+        this.showPages.splice(0, 3);
+        this.showPages.push((+this.showPages[this.showPages.length - 1] + 1).toString());
+        this.showPages.push((+this.showPages[this.showPages.length - 1] + 1).toString());
+        this.showPages.push((+this.showPages[this.showPages.length - 1] + 1).toString());
+      break;
+     case 2:
+        this.showPages.splice(0, 2);
+        this.showPages.push((+this.showPages[this.showPages.length - 1] + 1).toString());
+        this.showPages.push((+this.showPages[this.showPages.length - 1] + 1).toString());
+      break;
+      case 3:
+        this.showPages.splice(0, 1);
+        this.showPages.push((+this.showPages[this.showPages.length - 1] + 1).toString());
+      break;
+    }
+  }
+
+  public nextPagination(page: string) {
+    this.pageable.page = page;
+    this.dataTableGetAll();
+
+    let index = this.showPages.indexOf(((+page) - 1).toString());
+    if (index >= 3) {
+      this.showPages.splice(0, 1);
+      this.showPages.push((+this.showPages[this.showPages.length - 1] + 1).toString());
+    }
+  }
+
+  public previousPagination(page: string) {
+    this.pageable.page = page;
+    this.dataTableGetAll();
+    if (+page > 2) {
+      this.showPages.pop();
+      this.showPages.unshift((+this.showPages[0] - 1).toString());
+    }
+  }
+
   private dataTableGetAll(): void {
     this.getAll(this.pageable).subscribe(
       value => {
+        this.totalElements = value.totalElements;
         this.rows = value.content;
         let currentSize = this.pageable.size;
         this.pageable = value.pageable;
@@ -103,5 +188,8 @@ export class CustomDataTableComponent implements OnInit, OnDestroy {
         i--;
       }
     }
+
+    if (this.showPages.length <= 0)
+      this.showPages = this.pages.slice(0, 7);
   }
 }
